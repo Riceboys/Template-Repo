0cbde39afb3cbb581500852608f49ba2b8b31cf3
Minh Quan Do <mdo6180@gmail.com>
2023-04-16 00:36:19 -0400

    added startup script

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if environment name is provided
+if [ $# -eq 0 ]
+  then
+    echo "No environment name provided"
+    exit 1
+fi
+
+# Assign the first argument to env_name variable
+env_name=$1
+
+# Create a new Python virtual environment using the venv module
+python3 -m venv $env_name
+
+# Activate the virtual environment
+source $env_name/bin/activate
